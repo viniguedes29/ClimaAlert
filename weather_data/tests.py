1e508acb4b384d7f5e611c0be8e4e1f2b119defb
Francisco Vinicius Sousa Guedes <ra260440@barney.lab.ic.unicamp.br>
2024-09-19 10:29:02 -0300

    adicionando testes ao build, bem como um arquivo de teste

--- weather_data/tests.py
+++ weather_data/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class WeatherDataTests(TestCase):
+    def test_get_weather_data(self):
+        # Example test case for get_weather_data function
+        location = "New York"
+        print("Oi")
+        # Assuming the function returns a dictionary with weather details
+        self.assertEqual("z", "z")
