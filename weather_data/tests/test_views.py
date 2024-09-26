501f4e6ece4c0829d79c14282decacb5051e7eff
Aline Hesley <alinehesley281@gmail.com>
2024-09-25 21:18:06 -0300

    fix

--- weather_data/tests/test_views.py
+++ weather_data/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from unittest.mock import patch
 
+
 class WeatherHTMLTestCase(TestCase):
     def setUp(self):
         self.client = Client()
@@ -19,7 +20,9 @@ class WeatherHTMLTestCase(TestCase):
         mock_get.return_value.json.return_value = mock_response
 
         # Faz uma requisição GET para a view
-        response = self.client.get(reverse("weather_by_city_name"), {"city_name": "São Paulo"})
+        response = self.client.get(
+            reverse("weather_by_city_name"), {"city_name": "São Paulo"}
+        )
 
         # Verifica se o status da resposta é 200 (OK)
         self.assertEqual(response.status_code, 200)
@@ -37,7 +40,7 @@ class WeatherHTMLTestCase(TestCase):
         self.assertContains(response, "<p>Temperatura: 25°C</p>", html=True)
         self.assertContains(response, "<p>Humidade: 60%</p>", html=True)
         self.assertContains(response, "<p>Nuvens: 0%</p>", html=True)
-        
+
     @patch("weather_data.views.requests.get")
     def test_weather_page_without_data(self, mock_get):
         # Mock da resposta da API com falha
@@ -45,7 +48,9 @@ class WeatherHTMLTestCase(TestCase):
         mock_get.return_value.json.return_value = {}
 
         # Faz uma requisição GET para a view
-        response = self.client.get(reverse("weather_by_city_name"), {"city_name": "Cidade Inexistente"})
+        response = self.client.get(
+            reverse("weather_by_city_name"), {"city_name": "Cidade Inexistente"}
+        )
 
         # Verifica se o status da resposta é 200 (OK)
         self.assertEqual(response.status_code, 200)
@@ -54,4 +59,6 @@ class WeatherHTMLTestCase(TestCase):
         self.assertContains(response, "<h1>Tempo em Cidade Inexistente</h1>", html=True)
 
         # Verifica se a mensagem de "Nenhuma informação disponível" está presente
-        self.assertContains(response, "<p>Nenhuma informação disponível.</p>", html=True)
+        self.assertContains(
+            response, "<p>Nenhuma informação disponível.</p>", html=True
+        )
